47e564f09764075dd2f3c3cea905df678fea7caa
natanaeljn <natanaeljn@outlook.com.br>
2022-05-03 21:48:51 -0300

    criado a janela de adicionar novo departamento

--- Oficina/src/gui/DepartamentoController.java
+++ Oficina/src/gui/DepartamentoController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerta;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.DepartamentoLista;
 import model.services.ServicoDepartamento;
@@ -34,8 +43,9 @@ public class DepartamentoController implements Initializable{
 	
 	@FXML
 	
-	public void onBtNovo() {
-		System.out.println("novo");
+	public void onBtNovo(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartamentoForma.fxml", parentStage);
 	}
 	
 	public void setDepartamento(ServicoDepartamento servico) {
@@ -66,6 +76,22 @@ public class DepartamentoController implements Initializable{
 		List<DepartamentoLista> list = servico.findAll();
 		obsList= FXCollections.observableArrayList(list);
 		tableViewDepartamento.setItems(obsList);
+	}
+	private void createDialogForm(String absoluteName,Stage  parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane  = loader.load();
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("entre com os dados do departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerta.showAlert("IO excessao", "errro de carregamento", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
